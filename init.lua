52426916a59e80d6b465e283c8c08fafaa4a09d6
fheinrich <heinrichfelix03@gmail.com>
2025-09-17 18:45:57 +0200

    increase fontsize to 18

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.o.guifont = "Menlo:h16"
+vim.o.guifont = "Menlo:h18"
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
