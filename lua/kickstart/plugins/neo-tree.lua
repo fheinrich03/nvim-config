4e8654205b6ddac060ba97938fbd087d49c9897e
fheinrich <heinrichfelix03@gmail.com>
2025-09-20 15:55:23 +0200

    use neo-tree, indent, lint plugins

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -2,22 +2,28 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
+  "nvim-neo-tree/neo-tree.nvim",
+  version = "*",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { "<leader>e", ":Neotree focus<CR>", desc = "Focus [E]xplorer", silent = true },
+    { "<leader>te", ":Neotree toggle<CR>", desc = "Toggle [E]xplorer", silent = true },
   },
   opts = {
     filesystem = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = true,
+      },
       window = {
+        -- Keep space available as <leader>
         mappings = {
-          ['\\'] = 'close_window',
+          ["<space>"] = "<space>",
         },
       },
     },
