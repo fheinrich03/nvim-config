db475b15bc1c9542ae4a6f4976a65a07a7529821
fheinrich <heinrichfelix03@gmail.com>
2025-09-23 20:06:14 +0200

    cleanup

--- lua/custom/plugins/nvim-java.lua
+++ lua/custom/plugins/nvim-java.lua
@@ -1,10 +1,17 @@
 return {
   "nvim-java/nvim-java",
   config = function()
-    -- Must be called before lspconfig
+    -- Must be called before defining jdtls config
     require("java").setup()
 
-    -- Setup jdtls LSP
-    require("lspconfig").jdtls.setup({})
+    -- Define configuration for jdtls (empty here, extend if you need settings)
+    vim.lsp.config("jdtls", {
+      -- on_attach = function(client, bufnr) ... end,
+      -- capabilities = require("blink.cmp").get_lsp_capabilities(),
+      -- settings = { java = { ... } },
+    })
+
+    -- Enable the server
+    vim.lsp.enable("jdtls")
   end,
 }
