d6191bc31cc7ec9a993e4d35b714bc42d93624f4
Felix Heinrich <f.heinrich@srp-gmbh.de>
2025-10-23 14:17:19 +0200

    cleanup

--- lua/custom/plugins/transparent.lua
+++ lua/custom/plugins/transparent.lua
@@ -1,56 +1,27 @@
 return {
   "xiyaowong/transparent.nvim",
-  lazy = false, -- sofort laden (nicht erst bei einem Event)
+  lazy = false,    -- sofort laden (nicht erst bei einem Event)
   priority = 1000, -- früh laden, damit es vor anderen Themes greift
   enabled = true,
   config = function()
+    -- Optional, you don't have to run setup.
     require("transparent").setup({
+      -- table: default groups
       groups = {
-        "Normal",
-        "NormalNC",
-        "Comment",
-        "Constant",
-        "Special",
-        "Identifier",
-        "Statement",
-        "PreProc",
-        "Type",
-        "Underlined",
-        "Todo",
-        "String",
-        "Function",
-        "Conditional",
-        "Repeat",
-        "Operator",
-        "Structure",
-        "LineNr",
-        "NonText",
-        "SignColumn",
-        "CursorLine",
-        "CursorLineNr",
-        "StatusLine",
-        "StatusLineNC",
-        "EndOfBuffer",
+        'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+        'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+        'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+        'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
+        'EndOfBuffer',
       },
-      extra_groups = {
-        "NormalFloat",
-        "FloatBorder",
-        "WinSeparator",
-        "Pmenu",
-        "PmenuSbar",
-        "PmenuThumb",
-        "TelescopeNormal",
-        "TelescopeBorder",
-        "NeoTreeNormal",
-        "NeoTreeNormalNC",
-        "NvimTreeNormal",
-        "WhichKeyFloat",
-      },
-      exclude_groups = {}, -- falls du Gruppen ausnehmen willst
+      -- table: additional groups that should be cleared
+      extra_groups = {},
+      -- table: groups you don't want to clear
+      exclude_groups = {},
+      -- function: code to be executed after highlight groups are cleared
+      -- Also the user event "TransparentClear" will be triggered
       on_clear = function() end,
     })
-
-    -- Direkt Transparenz aktivieren
-    require("transparent").clear_prefix("lualine") -- optional, falls du lualine nutzt
+    vim.cmd("TransparentEnable")
   end,
 }
